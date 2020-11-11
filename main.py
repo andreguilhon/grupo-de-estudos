ac5e2beecbc30f5198bf8d9f208260f344ea320d
andreguilhon <andre.guilhon@gmail.com>
2020-11-11 18:59:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from xlrd import open_workbook
 from docx import Document
-from os import path, mkdir
+from os import path, mkdir, remove
 from datetime import datetime
 from locale import setlocale, LC_ALL, format_string
 from num2words import num2words
@@ -11,9 +11,18 @@ from glob import glob
 from time import sleep
 import comtypes.client
 from fpdf import FPDF 
+from tkinter import Tk, filedialog, Label, Grid, Button, Entry, OptionMenu, StringVar
 
 
-def create_modified_docx(template, planilha, ac, genero, output):
+class PeticaoEmLote:
+    modelo = ''
+    planilha = ''
+    ac = ''
+    genero = ''
+    resultado = ''
+
+
+def create_modified_docx(template, planilha, ac, genero, output, window):
     if genero.lower() == 'h':
         cargo = 'Procurador'
     elif genero.lower() == 'm':
@@ -25,13 +34,14 @@ def create_modified_docx(template, planilha, ac, genero, output):
     sheet = wb.sheet_by_index(0)
 
     for line in range(1, sheet.nrows - 1):
-    # for line in range(1, 3):
+    # for line in range(1, 2):
         document = Document(template)
         uf = sheet.cell_value(line, 0)
         cnpj = sheet.cell_value(line, 1)
         filial = "%s.%s.%s/%s-%s" % (cnpj[0:2], cnpj[2:5], cnpj[5:8], cnpj[8:12], cnpj[12:14])
         ie = sheet.cell_value(line, 2)
         local = sheet.cell_value(line, 3)
+        local_caps = local.upper()
         endereco = sheet.cell_value(line, 4)
         mes = sheet.cell_value(line, 5)
         ano = sheet.cell_value(line, 6)
@@ -48,6 +58,7 @@ def create_modified_docx(template, planilha, ac, genero, output):
                                 '{{filial}}': filial,
                                 '{{ie}}': ie,
                                 '{{local}}': local,
+                                '{{local_caps}}': local_caps,
                                 '{{endereco}}': endereco,
                                 '{{mes}}': mes,
                                 '{{ano}}': ano,
@@ -65,7 +76,10 @@ def create_modified_docx(template, planilha, ac, genero, output):
         result_name = f'001-peticao'
         document.save(f'{path.join(result_path,result_name)}.docx')
         convert_to_pdf(f'{path.join(result_path,result_name)}.docx', f'{path.join(result_path,result_name)}.pdf')
-        pdf_cat(get_pdfs([result_path]), output_stream=open(f'{path.join(result_path,result_name)}_pronta.pdf', 'w+b'))
+        if path.isfile(f'{path.join(result_path,result_name)}_pronta.pdf'):
+            remove(f'{path.join(result_path,result_name)}_pronta.pdf')
+        pdf_cat(get_pdfs(result_path), output_stream=open(f'{path.join(result_path,result_name)}_pronta.pdf', 'w+b'))
+    Label(window, text=f"Todos os períodos finalizados").grid(row=line+5, column=0)
 
 
 def uf_por_extenso(uf):
@@ -200,19 +214,16 @@ def docx_replace(doc, data):
 
 
 def get_pdfs(list_to_add):
-    all_files = []
-    for _file in list_to_add:
-        if path.isdir(_file):
-            all_files += glob(f'{_file}/*')
-        elif path.isfile(_file):
-            all_files.append(_file)
-    
-    all_files += sorted(glob("pdf/*.pdf"))
+    all_files = [path.join(list_to_add,'001-peticao.pdf')]
+    pdfs = sorted(glob("./pdf/*.pdf"))
+    all_files += pdfs
+    folder_glob = glob(f'{list_to_add}/*') 
+    folder_glob_reduzido = [s for s in folder_glob if not s.__contains__('001-peticao')]
+    all_files += folder_glob_reduzido
     return all_files
 
 
 def pdf_cat(input_files, output_stream):
-    print(input_files)
     input_streams = []
     try:
         # First open all the files, then produce the output file, and
@@ -225,12 +236,18 @@ def pdf_cat(input_files, output_stream):
             if file_extension.lower() == '.pdf':
                 input_streams.append(open(input_file, 'rb'))
             elif file_extension.lower() == '.xlsx':
+                if path.isfile(filename+'.pdf'):
+                    remove(filename+'.pdf')
                 convert_xlsx_to_pdf(input_file, filename+'.pdf')
                 input_streams.append(open(filename+'.pdf', 'rb'))
             elif file_extension.lower() == '.csv':
+                if path.isfile(filename+'.pdf'):
+                    remove(filename+'.pdf')
                 txt_to_pdf(input_file, filename+'.csv.pdf')
                 input_streams.append(open(filename+'.csv.pdf', 'rb'))
             elif file_extension.lower() == '.txt':
+                if path.isfile(filename+'.pdf'):
+                    remove(filename+'.pdf')
                 txt_to_pdf(input_file, filename+'.txt.pdf')
                 input_streams.append(open(filename+'.txt.pdf', 'rb'))
         writer = PdfFileWriter()
@@ -262,20 +279,21 @@ def convert_to_pdf(input_file, output_file):
 def txt_to_pdf(input_file, output_file):
     # save FPDF() class into a  
     # variable pdf 
-    pdf = FPDF('P', 'mm','A4') 
+    pdf = FPDF('L', 'mm','A4') 
     
     # Add a page 
     pdf.add_page() 
     
     # set style and size of font  
     # that you want in the pdf 
-    pdf.set_font("Arial", size = 8) 
+    pdf.set_font("Arial", size = 6) 
     
     # open the text file in read mode 
     with open(input_file, "r", newline='', encoding='iso-8859-1') as f:
         # insert the texts in pdf 
         for x in f: 
-            pdf.cell(200,10, border=0, txt = x, ln=1) 
+            pdf.multi_cell(220,6, border=0, txt = x)
+
     
     # save the pdf with name .pdf 
     pdf.output(output_file)    
@@ -294,11 +312,57 @@ def convert_xlsx_to_pdf(input_file, output_file):
 
     app.Quit()
 
+def openfilename(peticao_em_lote, atribute, extensions):
+    setattr(peticao_em_lote, atribute,filedialog.askopenfilename(filetypes=extensions))
+
+def opendirectory(peticao_em_lote):
+    peticao_em_lote.resultado = filedialog.askdirectory()
+
+def execute(peticao_em_lote, window):
+    try:
+        Label(window, text="Criando as petições. Aguarde.").grid(row=6, column=0)
+        create_modified_docx(peticao_em_lote.modelo, peticao_em_lote.planilha, peticao_em_lote.ac.get(), peticao_em_lote.genero.get(), peticao_em_lote.resultado, window)
+        Label(window, text="Todas as petições criadas com sucesso.").grid(row=7, column=0)
+    except Exception as e:
+        print(e)
+        Label(window, text=f"Ocorreu um erro ao criar as petições:").grid(row=6)
+        Label(window, text=e, width=25, wraplength=150).grid(row=7)
+        pass
+    print('Finalizado')
+
 if __name__ == '__main__':
-    # txt_to_pdf('teste.txt', 'output.pdf')
-    template = input("Digite o caminho completo do arquivo com a petição de modelo:\n")
-    planilha = input("Digite o caminho completo da planilha com os dados a serem importados: \n")
-    ac = input("Digite o nome do destinatário:\n")
-    genero = input("Digite o genero do parceiro (H ou M):\n")
-    output = input("Digite o caminho base onde deseja salvar os resultados (será salvo em Caminho\CNPJ\Periodo, 33041260104962\\022017\\peticao_pronta.pdf por exemplo):\n")
-    create_modified_docx(template, planilha, ac, genero, output)
+    peticao_em_lote = PeticaoEmLote()
+    root = Tk()
+    root.title('Criar petições em lote')
+    root.geometry('500x500')
+
+    label_modelo = Label(root, text="Selecione o modelo")
+    label_modelo.grid(row=0,column=0)
+    modelo = Button(root, text="Selecionar", command=lambda: openfilename(peticao_em_lote, 'modelo', [('Word', '*.docx')]), width=20)
+    modelo.grid(row=0,column=1)
+
+    label_planilha = Label(root, text="Selecione a planilha")
+    label_planilha.grid(row=1,column=0)
+    planilha = Button(root, text="Selecionar", command=lambda: openfilename(peticao_em_lote, 'planilha', [('Excel', '*.xlsx')]), width=20)
+    planilha.grid(row=1,column=1)
+
+    label_ac = Label(root, text="Digite o nome do destinatário:")
+    label_ac.grid(row=2,column=0)
+    peticao_em_lote.ac = Entry(root, width=25)
+    peticao_em_lote.ac.grid(row=2,column=1)
+
+    label_genero = Label(root, text="Selecione o genero do parceiro")
+    label_genero.grid(row=3,column=0)
+    peticao_em_lote.genero = StringVar()
+    peticao_em_lote.genero.set('H')
+    genero = OptionMenu(root, peticao_em_lote.genero, 'H', 'M')
+    genero.grid(row=3,column=1)
+
+    label_resultado = Label(root, text="Selecione o diretorio de destino")
+    label_resultado.grid(row=4,column=0)
+    peticao_em_lote.resultado = Button(root, text="Selecionar", command=lambda: opendirectory(peticao_em_lote), width=20)
+    peticao_em_lote.resultado.grid(row=4,column=1)
+
+    executar = Button(root, text="Executar", command=lambda: execute(peticao_em_lote, root), width=20)
+    executar.grid(row=5, column=0)
+    root.mainloop()
